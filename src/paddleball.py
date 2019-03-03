b00eb9c824a5e50853861f998127e256fab9d9e3
Oleksandr Budiak <ambudyak@gmail.com>
2019-03-03 20:54:09 +0200

    paddleball.py v1.1

--- src/paddleball.py
+++ src/paddleball.py
@@ -21,7 +21,7 @@ class Ball:
     def hit_paddle(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
         if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
-            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+            if pos[3] >= paddle_pos[1] <= paddle_pos[3]:
                 return True
         return False
 
